7af2542440578341877cec05461a5bced94992fb
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-04-17 18:49:10 +0530

    Create eplace each element of the array by its rank in the array.py

---
+++ eplace each element of the array by its rank in the array.py
@@ -0,0 +1,13 @@
+arr=[15,10,8]
+sorted_arr = sorted(arr)
+    
+
+rank_dict = {val: i+1 for i, val in enumerate(sorted_arr)
+result = []
+for val in arr:
+      
+    result.append(rank_dict[val])
+    
+
+print(result)
+
