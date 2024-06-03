328bd8ffe4fff1038cd858b34874309ecd208682
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-06-03 21:05:47 +0530

    Create Replacing a particular word with another word in a string.py

---
+++ Replacing a particular word with another word in a string.py
@@ -0,0 +1,6 @@
+original_string = "Hello world, welcome to the world of Python."
+word_to_replace = "world"
+replacement_word = "universe"
+new_string = original_string.replace(word_to_replace, replacement_word)
+
+print(new_string)
