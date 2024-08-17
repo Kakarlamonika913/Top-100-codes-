f8c38d2030bf53bd3b22b6e032ad22525196a0be
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-08-17 21:44:10 +0530

    Create reverse pyramid.py

---
+++ reverse pyramid.py
@@ -0,0 +1,9 @@
+def pyramid(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ",end=" ")
+        for j in range(2*(n-i)-1):
+               print("*",end=" ")
+        print()
+size=5
+pyramid(size)
