a4db247dd6e9901005a5aac0532669b22bb6529f
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-08-31 21:27:01 +0530

    Create pattern 19.py

---
+++ pattern 19.py
@@ -0,0 +1,13 @@
+def right_angle(n):
+    for i in range(0,n+1):
+        print("*" *i)
+    for i in range(n,0,-1):
+        print("*" *i)
+    print()
+    for i in range(0,n+1):
+        print("*" *i)
+    for i in range(n,0,-1):
+        print("*" *i)
+    print()
+size=5
+right_angle(size)
