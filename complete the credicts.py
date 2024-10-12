b2bea79bbfc196ca4555da3de7aa73aa131c106e
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-10-12 21:23:40 +0530

    Create complete the credicts.py

---
+++ complete the credicts.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for i in range(int(input())):
+    a=int(input())
+    if(a>65):
+        print("Overload")
+    elif a<35:
+        print('Underload')
+    else: print('Normal')
