65054993cd17c9f50def40d024928724a7a96dec
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-11-04 18:46:34 +0530

    Create sum of two prime numbers.py

---
+++ sum of two prime numbers.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    for i in range(n):
+        if n%i==0:
+            flag=0
+            break
+        return is_prime
+    for i in range(2,n):
+        if is_prime(i)==1:
+            if is_prime(n-1)==1:
+                print(n,i,n-1)
+                flag=0
+    if flag==1:
+        print("cannot be")
+                
