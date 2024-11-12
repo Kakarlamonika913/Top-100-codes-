1770a55d9d93411dd9c73c44f10d0e3b90bd4189
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-11-12 19:07:39 +0530

    Create Star.c

---
+++ Star.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+
+	int n;
+
+	
+
+	// Do not print anything before taking input
+
+	scanf("%d",&n);
+
+	printf("%d",n);
+
+	// Print the value of n
+
+	
+
+}
+
