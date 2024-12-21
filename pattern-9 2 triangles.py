ed4ca7d703d59f9c8f68037b887055c21d463941
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2024-12-21 19:32:12 +0530

    Create pattern-9 2 triangles.py

---
+++ pattern-9 2 triangles.py
@@ -0,0 +1,9 @@
+a, b = 3, 5
+for i in range(a):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+for i in range(b):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
