1de0015a446ba4986f0177d59d234ca37d7e1e36
Kakarlamonika913 <135260218+Kakarlamonika913@users.noreply.github.com>
2025-02-14 20:44:19 +0530

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+def binary_search(arr,target):
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+arr=[1,3,4,2]
+target=3
+print(binary_search(arr,target))
